76ce5d9b3ba4cd3caa1c87c5a46a33b37b3fb9f7
Prathwik <75462688+prathwiknaik08@users.noreply.github.com>
2025-08-07 16:29:01 +0530

    Update ProductCard.tsx

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -6,7 +6,6 @@ import React from 'react'
 interface Props {
   product: Product;
 }
- 
 const ProductCard = ({ product }: Props) => {
   return (
     <Link href={`/products/${product._id}`} className="product-card">
